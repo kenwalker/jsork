90d0f2ca86bbd33370fecb6934c8732462659e35
Ken Walker <kwalker@Kens-MacBook-Pro.local>
2020-03-01 22:01:25 -0500

    Add Active but Retired report

---
+++ docs/assets/js/activeRetired.js
@@ -0,0 +1,167 @@
+/* eslint-disable no-unused-vars */
+/* global jsork, $ */
+
+var playerList = null;
+var playerContent = '';
+
+function kingdomSelect(event, ui) {
+  playerList = [];
+  $('.allresults').attr('hidden', true);
+  $('table').find('tr:gt(0)').remove();
+  $('.noplayers').text(' ');
+  if (event.target.value === '0') {
+    return;
+  }
+  document.getElementById('kingdom').disabled = true;
+  document.getElementById('park').disabled = true;
+  var select = document.getElementById('park');
+  for (var i = select.options.length - 1; i >= 0; i--) {
+    select.options[i] = null;
+  }
+  jsork.kingdom.getParks(parseInt(event.target.value, 10)).then(function(data) {
+    data.sort(function(a, b) {
+      return a.Name.toLowerCase().localeCompare(b.Name.toLowerCase());
+    });
+    var kSelect = $('#park');
+    var emptyOption = $('<option>');
+    emptyOption.html('Choose a Park');
+    emptyOption.val(0);
+    kSelect.append(emptyOption);
+    data.forEach(function(park) {
+      if (park.Active === 'Active') {
+        var option = $('<option>');
+        option.html(park.Name);
+        option.val(park.ParkId);
+        kSelect.append(option);
+      }
+    });
+    $('#parkselect').attr('hidden', false);
+    document.getElementById('kingdom').disabled = false;
+    document.getElementById('park').disabled = false;
+  });
+}
+
+function parkSelect(event, ui) {
+  $('.allresults').attr('hidden', true);
+  $('table').find('tr:gt(0)').remove();
+  $('.noplayers').text('');
+  playerList = [];
+  playerContent = '';
+  if (event.target.value === '0') {
+    return;
+  }
+  document.getElementById('kingdom').disabled = true;
+  document.getElementById('park').disabled = true;
+  $('.printtitle').text($('#kingdom option:selected').text() + ' - ' + $('#park option:selected').text());
+  $('.generateddate').text('Generated on ' + new Date().toDateString());
+
+  $('.working').attr('hidden', false);
+  $('.working').text('Getting players....');
+  jsork.park.getPlayers(parseInt(event.target.value, 10), jsork.filters.INACTIVE).then(function(data) {
+    var playersLeft = data.length;
+    if (playersLeft === 0) {
+      document.getElementById('kingdom').disabled = false;
+      document.getElementById('park').disabled = false;
+      $('.working').attr('hidden', true);
+      $('.noplayers').text('There are no inactive players');
+      return;
+    }
+    data.forEach(function(player) {
+      jsork.player.getLastAttendance(player.MundaneId).then(function(lastAttendance) {
+        if (lastAttendance.length > 0 && moment(lastAttendance[0].Date) > moment().subtract(8, 'months')) {
+            player.lastAttendance = lastAttendance[0].Date;
+            playerList.push(player);
+            $('.working').text('Number of players left to check ' + playersLeft);
+            if (--playersLeft <= 0) {
+                donePlayers();  
+            }
+        } else {
+          $('.working').text('Number of players left to check ' + playersLeft);
+          if (--playersLeft <= 0) {
+            donePlayers();
+          }
+        }
+      });
+    });
+  });
+}
+
+function donePlayers() {
+  if (playerList.length === 0) {
+    document.getElementById('kingdom').disabled = false;
+    document.getElementById('park').disabled = false;        
+    // $('.noplayers').text('Generated on ' + new Date().toDateString());
+    $('.working').attr('hidden', true);
+    $('.noplayers').text('There are no players marked INACTIVE that have played in the last 8 months');
+    return;
+  }
+  playerList.sort(function(a, b) {
+    return a.Persona.toLowerCase().localeCompare(b.Persona.toLowerCase());
+  });
+  var lastPlayer = null;
+  playerList.forEach(function(aPlayer) {
+    var playerHTMLLine = '';
+    var playerLine = (aPlayer.Persona || 'No persona for ID ' + aPlayer.MundaneId) + '\t';
+    if (lastPlayer && lastPlayer.Persona === aPlayer.Persona) {
+      playerHTMLLine += '<tr><td></td>';
+    } else {
+      playerHTMLLine += '<tr><td><a href="https://ork.amtgard.com/orkui/index.php?Route=Player/index/' +
+      aPlayer.MundaneId + '">' +
+      (aPlayer.Persona || 'No persona for ID ' + aPlayer.MundaneId) + '</a></td>';
+    }
+    playerHTMLLine += "<td>" + aPlayer.lastAttendance + "</td></tr>";
+    $('#playerTable').append(playerHTMLLine);
+    playerContent += playerLine + '\r\n';
+    lastPlayer = aPlayer;
+  });
+  $('.working').attr('hidden', true);
+  $('.allresults').attr('hidden', false);
+  document.getElementById('kingdom').disabled = false;
+  document.getElementById('park').disabled = false;
+}
+
+function initKingdoms() {
+  $('#parkselect').attr('hidden', true);
+  $('.working').attr('hidden', true);
+  jsork.kingdom.getKingdoms().then(function(data) {
+    var kSelect = $('#kingdom');
+    var emptyOption = $('<option>');
+    emptyOption.html('Choose a Kingdom/Principality');
+    emptyOption.val(0);
+    kSelect.append(emptyOption);
+    data.forEach(function(kingdom) {
+      var option = $('<option>');
+      option.html(kingdom.KingdomName);
+      option.val(kingdom.KingdomId);
+      kSelect.append(option);
+    });
+    $('#kingdom').selectmenu('option', 'disabled', false);
+    $('#kingdom').show();
+  });
+}
+
+function startUp() {
+  $('#kingdom').selectmenu();
+  $('#kingdom').on('change', kingdomSelect);
+  $('#park').on('change', parkSelect);
+  initKingdoms();
+}
+
+function copyTextToClipboard(str) {
+  var el = document.createElement('textarea');
+  el.value = str;
+  document.body.appendChild(el);
+  el.select();
+  document.execCommand('copy');
+  document.body.removeChild(el);
+}
+
+function copyToClipboard() {
+  var allCSV = 'Persona\tLast Playeed\r\n';
+  allCSV += playerContent;
+  copyTextToClipboard(allCSV);
+}
+
+$(document).ready(function() {
+  startUp();
+});
