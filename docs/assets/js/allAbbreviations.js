b0a49fd0c2ee5ac58619166e68d8a6f07514fb93
Ken Walker <ken_walker@me.com>
2025-06-07 15:38:01 -0400

    Sort the Kingdoms properly

--- docs/assets/js/allAbbreviations.js
+++ docs/assets/js/allAbbreviations.js
@@ -3,6 +3,48 @@ var allKingdoms = [];
 function startUp() {
     jsork.kingdom.getKingdoms().then(function(kingdoms) {
         allKingdoms = kingdoms;
+        allKingdoms.sort(function (kingdomA, kingdomB) {
+            var a = kingdomA.KingdomName;
+            var b = kingdomB.KingdomName;
+            if (a.startsWith("Kingdom of the ")) {
+                a = a.slice("Kingdom of the ".length)
+            }
+            if (a.startsWith("The Kingdom of the ")) {
+                a = a.slice("The Kingdom of the ".length)
+            }
+            if (a.startsWith("The Empire of the ")) {
+                a = a.slice("The Empire of the ".length)
+            }
+            if (a.startsWith("The Kingdom of ")) {
+                a = a.slice("The Kingdom of ".length)
+            }
+            if (a.startsWith("The Principality of the ")) {
+                a = a.slice("The Principality of the ".length)
+            }
+            if (a.startsWith("The ")) {
+                a = a.slice("The ".length)
+            }
+            if (b.startsWith("Kingdom of the ")) {
+                b = b.slice("Kingdom of the ".length)
+            }
+            if (b.startsWith("The Kingdom of the ")) {
+                b = b.slice("The Kingdom of the ".length)
+            }
+            if (b.startsWith("The Empire of the ")) {
+                b = b.slice("The Empire of the ".length)
+            }
+            if (b.startsWith("The Kingdom of ")) {
+                b = b.slice("The Kingdom of ".length)
+            }
+            if (b.startsWith("The Principality of the ")) {
+                b = b.slice("The Principality of the ".length)
+            }
+            if (b.startsWith("The ")) {
+                b = b.slice("The ".length)
+            }
+            console.log(a + " : " + b);
+            return b.toLowerCase().localeCompare(a.toLowerCase());
+        });
         nextKingdom();
     });
 }
@@ -14,6 +56,9 @@ function nextKingdom() {
     }
     var aKingdom = allKingdoms.pop();
     jsork.kingdom.getParks(aKingdom.KingdomId).then(function(kingdomParks) {
+        kingdomParks.sort(function (a, b) {
+            return a.Name.toLowerCase().localeCompare(b.Name.toLowerCase());
+        });
         kingdomParks.forEach(function(aPark) {
             var abbreviationsHTMLLine = '<tr>';
             abbreviationsHTMLLine += '<td> ' + aKingdom.KingdomName || '' + '</td>';
